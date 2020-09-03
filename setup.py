a5018783081e1eb42a0014216e227ab0fe7298dd
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2020-09-02 22:16:19 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+#! /usr/bin/env python3
+from setuptools import setup
+import sys
+if sys.version_info[0] < 3: raise RuntimeError("Sorry, you must use Python 3")
+# The directory containing this file
+import pathlib
+import os
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+setup(
+    name                =   'masswappalyzer',
+    description         =   "Run Wappalyzer asynchronously on a list of URLs and generate a excel file containing all results.",
+    url                 =   "https://github.com/tristanlatr/MassWappalyzer",
+    maintainer          =   "tristanlatr",
+    entry_points        =   {'console_scripts': ['masswappalyzer = masswappalyzer:main'],},
+    classifiers         =   ["Programming Language :: Python :: 3"],
+    license             =   'Apache License 2.0',
+    long_description    =   README,
+    long_description_content_type   =   "text/markdown",
+    install_requires    =   ['XlsxWriter', 'tqdm']
+)
